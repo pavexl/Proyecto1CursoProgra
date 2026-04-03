59a430088ecc6b0bc8e93593147f142b48db0ef7
pavelgcs <pavel20ganchala@gmail.com>
2026-04-02 20:31:52 -0500

    Agregar prototipos de funciones e iniciar el programa principal

--- proyecto1.cpp
+++ proyecto1.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 using namespace std;
 
+//====================== PROTOTIPO DE FUNCIONES ======================
+
+void registrar(string nombres[], double notas[], int &contador);
+void mostrar(string nombres[], double notas[], int contador);
+void calcularPromedio(double notas[], int contador);
+void buscar(string nombres[], double notas[], int contador);
+void mayorNota(string nombres[], double notas[], int contador);
+
+//====================== PROGRAMA PRINCIPAL ======================
+int main() {
+
+    // Declaración de arreglos y variables
+    const int MAX_ESTUDIANTES = 100;
+    string nombres[MAX_ESTUDIANTES];
+    double notas[MAX_ESTUDIANTES];
+    int contador = 0;
+    int opcion;
+
+    
+
+
+
+
+
+}
